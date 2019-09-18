4e2eee959570ba5bc34061787fd28cd4f2199f45
Joseph Watts <josephcw@live.com>
2019-09-18 12:02:44 -0500

    Add label next to sliders, change on input

--- main.js
+++ main.js
@@ -6,15 +6,25 @@ const updateColor = () => {
   const canvasElement = document.querySelector('#canvas')
   canvasElement.style.backgroundColor = `rgb(${r},${g},${b})`
   canvasElement.style.border = "thin solid black"
+
+  const rlabel = document.querySelector('#r-value')
+  const glabel = document.querySelector('#g-value')
+  const blabel = document.querySelector('#b-value')
+  rlabel.innerHTML = r
+  glabel.innerHTML = g
+  blabel.innerHTML = b
 }
 
 // configure event listeners
-document.querySelector('#red').addEventListener('click', updateColor)
-document.querySelector('#blue').addEventListener('click', updateColor)
-document.querySelector('#green').addEventListener('click', updateColor)
-document.querySelector('#red').addEventListener('touchend', updateColor)
-document.querySelector('#blue').addEventListener('touchend', updateColor)
-document.querySelector('#green').addEventListener('touchend', updateColor)
+// document.querySelector('#red').addEventListener('click', updateColor)
+// document.querySelector('#blue').addEventListener('click', updateColor)
+// document.querySelector('#green').addEventListener('click', updateColor)
+// document.querySelector('#red').addEventListener('touchend', updateColor)
+// document.querySelector('#blue').addEventListener('touchend', updateColor)
+// document.querySelector('#green').addEventListener('touchend', updateColor)
+document.querySelector('#red').addEventListener('input', updateColor)
+document.querySelector('#blue').addEventListener('input', updateColor)
+document.querySelector('#green').addEventListener('input', updateColor)
 
 // call the handler 
 updateColor() 
